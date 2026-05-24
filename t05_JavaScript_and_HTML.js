ff7e6e5b40c536ba3baa739a7c10a8bb18d0368f
24448mo-lab <24448mo@hvhs.school.nz>
2026-05-24 21:25:21 +0000

    May 24, 2026, 9:25 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,4 +1,4 @@
 
 
-const OUTPUT = document.getElementById("JavaScriptOutput");
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
