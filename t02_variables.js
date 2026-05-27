51c1d86b703374967d8cb0b1bacb14985d008e70
24448mo-lab <24448mo@hvhs.school.nz>
2026-05-27 00:56:01 +0000

    May 27, 2026, 12:56 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,9 @@
-/* Paste your code from task 1 here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t01_introduction.js")
+console.log("sadasdadsa")
+
+// This is a single line comment
+
