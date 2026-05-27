2faf5dcd150a8ec455c814e794946c7f87dc524c
24448mo-lab <24448mo@hvhs.school.nz>
2026-05-27 01:09:03 +0000

    May 27, 2026, 1:09 AM

--- t03_maths.js
+++ t03_maths.js
@@ -13,10 +13,12 @@ let randomMoney = 60;
 let actualYear = 2026; 
 
 console.log("Hi " + randomName);
-console.log("You have " + randomMoney);
 console.log("As of " + actualYear + " you are " + randomAge);
 console.log ("You were born in " + (actualYear - randomAge));
 console.log ("In 10 years you will be " + (randomAge + 10) + " years old");
 console.log ("In 10 years you will be " + (randomAge + 10) + " years old");
+console.log("You have " + randomMoney);
+console.log ("In 10 years you will be " + (randomAge + 10) + " years old");
+
 
 
