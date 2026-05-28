26bd918f8c9f1ada7f2b9f70003c4a2bc1b21594
24448mo-lab <24448mo@hvhs.school.nz>
2026-05-28 02:56:49 +0000

    May 28, 2026, 2:56 AM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,19 @@
-/* Paste your code from task 7 here*/
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Hello Maite</h2>";
+OUTPUT.innerHTML += "<h2>Goodbye</h2>";
+
+function welcome() {
+    OUTPUT.innerHTML += "<h1>Welcome to the shop</h1>";
+}
+
+
+welcome()
+
+function displayProduct(_name, _price) {
+    OUTPUT.innerHTML += "<h2>"+ _name + ": $"+ _price + "</h2>";
+}
+
+displayProduct ("Chocolate bar", 4)
+displayProduct ("Chips", 3)
+displayProduct ("Drink", 2.50)
